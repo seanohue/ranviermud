ea56e896079870a1adcbb5e87e437ccc2761b7a3
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2017-03-18 00:36:30 -0400

    Here is that lazy class impl.

---
+++ bundles/myelin-classes/classes/base.js
@@ -0,0 +1,22 @@
+'use strict';
+
+/**
+ * This example definition of a class file is a guideline. The class system is
+ * left intentionally vague so as to not be too opinionated. Class files are
+ * assumed to be js files instead of blank yaml files just to future proof the
+ * bundle-loading of class files in case someone wants extra functionality in
+ * their classes.
+ */
+module.exports = srcPath => {
+  return {
+    name: 'None',
+    description: 'There are no classes.',
+    //TODO: Have ability gains be based on attribute prerequisites.
+    abilityTable: {
+      3: { skills: ['rend'] },
+      5: { skills: ['lunge'] },
+      7: { skills: ['shieldblock'] },
+     10: { skills: ['secondwind'] },
+    }
+  };
+};
---
+++ bundles/myelin-classes/skills/heal.js
@@ -0,0 +1,54 @@
+'use strict';
+
+/**
+ * Basic cleric spell
+ */
+module.exports = (srcPath) => {
+  const Broadcast = require(srcPath + 'Broadcast');
+  const Heal = require(srcPath + 'Heal');
+  const SkillType = require(srcPath + 'SkillType');
+
+  const healPercent = 300;
+  const energyCost = 40;
+
+  function getHeal(player) {
+    return player.getAttribute('intellect') * (healPercent / 100);
+  }
+
+  return {
+    name: 'Heal',
+    type: SkillType.SPELL,
+    requiresTarget: true,
+    initiatesCombat: false,
+    targetSelf: true,
+    resource: {
+      attribute: 'energy',
+      cost: energyCost,
+    },
+    cooldown: 10,
+
+    run: state => function (args, player, target) {
+      const heal = new Heal({
+        attribute: 'health',
+        amount: getHeal(player),
+        attacker: player,
+        source: this
+      });
+
+      if (target !== player) {
+        Broadcast.sayAt(player, `bold>You call upon to the light to heal ${target.name}'s wounds.</bold>`);
+        Broadcast.sayAtExcept(player.room, `bold>${player.name} calls upon to the light to heal ${target.name}'s wounds.</bold>`, [target, player]);
+        Broadcast.sayAt(target, `bold>${player.name} calls upon to the light to heal your wounds.</bold>`);
+      } else {
+        Broadcast.sayAt(player, "<bold>You call upon to the light to heal your wounds.</bold>");
+        Broadcast.sayAtExcept(player.room, `bold>${player.name} calls upon to the light to heal their wounds.</bold>`, [player, target]);
+      }
+
+      heal.commit(target);
+    },
+
+    info: (player) => {
+      return `Call upon the light to heal your target's wounds for ${healPercent}% of your Intellect.`;
+    }
+  };
+};
