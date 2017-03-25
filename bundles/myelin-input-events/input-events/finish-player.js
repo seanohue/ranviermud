4424f5b562deadcb399347b8e6cc591ee860f91f
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2017-03-25 08:29:26 -0400

    Set metas on char creation.

--- bundles/myelin-input-events/input-events/finish-player.js
+++ bundles/myelin-input-events/input-events/finish-player.js
@@ -22,7 +22,12 @@ module.exports = (srcPath) => {
       player.room = room;
       player.save();
 
-      // reload from manager so events are set
+      // Myelin meta-stats.
+      player.setMeta('abilities', []);
+      player.setMeta('abilityPoints', 0);
+      player.setMeta('attributePoints', 0);
+
+      // Reload from manager so events are set
       player = state.PlayerManager.loadPlayer(state, player.account, player.name);
       player.socket = socket;
 
