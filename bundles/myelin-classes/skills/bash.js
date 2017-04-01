6b12ea27f72743913a61d1052c88d784ed65ce5c
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2017-03-31 20:08:41 -0400

    Finish bash command by adding broadcasts and adding effect to target. Give tough just the bash skill but they can buy secondwind soon if they want.

--- bundles/myelin-classes/skills/bash.js
+++ bundles/myelin-classes/skills/bash.js
@@ -7,13 +7,18 @@ module.exports = (srcPath) => {
   const Broadcast = require(srcPath + 'Broadcast');
   const Damage = require(srcPath + 'Damage');
   const SkillType = require(srcPath + 'SkillType');
+  const Random = require(srcPath + 'RandomUtil');
 
   const damagePercent = 150;
   const energyCost = 50;
   const stunPercent = 20;
 
-  function getDamage(player, energyBonus) {
+  function getDamage(player) {
     return player.calculateWeaponDamage() * (damagePercent / 100) + player.getMaxAttribute('might');
+  }
+
+  function getDuration(player) {
+    return Math.ceil(player.getMaxAttribute('might') / 5);
   }
 
   return {
@@ -53,16 +58,28 @@ module.exports = (srcPath) => {
       effect.skill = this;
       effect.attacker = player;
 
-      Broadcast.sayAt(player, '<red>You shift your feet and let loose a mighty attack!</red>');
-      Broadcast.sayAtExcept(player.room, `<red>${player.name} lets loose a lunging attack on ${target.name}!</red>`, [player, target]);
+      const isStunned = Random.probability(20);
+
+      Broadcast.sayAt(player, `<red>You bash ${target.name} viciously, knocking them back!</red>`);
+      Broadcast.sayAtExcept(player.room, `<red>${player.name} viciously bashes ${target.name}!</red>`, [player, target]);
       if (!target.isNpc) {
-        Broadcast.sayAt(target, `<red>${player.name} lunges at you with a fierce attack!</red>`);
+        Broadcast.sayAt(target, `<red>${player.name} bashes you with their weapon!</red>`);
       }
+
+      if (isStunned) {
+        Broadcast.sayAt(player, `<yellow>${target.name} is stunned!</yellow>`);
+        Broadcast.sayAtExcept(player.room, `<yellow>${player.name} stuns ${target.name}!</yellow>`, [player, target]);
+        if (!target.isNpc) {
+          Broadcast.sayAt(target, `<yellow>${player.name}'s attack stuns you!</yellow>`);
+        }
+        target.addEffect(effect);
+      }
+
       damage.commit(target);
     },
 
     info: (player) => {
-      return `Make a strong attack against your target dealing <bold>${damagePercent}%</bold> weapon damage, plus your Might, with a <bold>${stunPercent}</bold> chance to stun.`;
+      return `Make a strong attack against your target dealing <bold>${damagePercent}%</bold> weapon damage, plus your Might, with a <bold>${stunPercent}</bold> chance to stun for <bold>${getDuration(player)}</bold> seconds.`;
     }
   };
 };
