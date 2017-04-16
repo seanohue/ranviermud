1ddb1152ea395b032bb497ceed6adff39d2cadc6
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2017-04-16 13:28:03 -0400

    Calculate karma based on # of kills, strongest defeated, and level.

---
+++ bundles/myelin-combat/lib/calculateKarma.js
@@ -0,0 +1,10 @@
+module.exports = function calculateKarma(player) {
+  const levelKarma = Math.floor(player.level / 3);
+  const killsKarma = Math.floor(player.getMeta('kills') / 50);
+  const strongestDefeated = player.getMeta('strongestDefeated');
+  const strongestDefeatedKarma = strongestDefeated ?
+    Math.floor(strongestDefeated.level / 8) : 0;
+
+  //TODO: Something about quests too.
+  return levelKarma + killsKarma + strongestDefeatedKarma;
+}
