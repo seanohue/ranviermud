6df84fb9d735d7e9df540a10d014b816a7271da1
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2017-05-31 19:24:52 -0400

    Add script for random noise.

--- bundles/myelin-areas/areas/spire.intro/scripts/rooms/1-tomb.js
+++ bundles/myelin-areas/areas/spire.intro/scripts/rooms/1-tomb.js
@@ -2,17 +2,12 @@
 
 module.exports = (srcPath) => {
   const Broadcast = require(srcPath + 'Broadcast');
-
-  return  {
+  const Random = require(srcPath + 'RandomUtil');
+  return {
     listeners: {
-      examine: state => function (player, examinable) {
-        switch (examinable) {
-          case 'cylinders':
-            return Broadcast.sayAt(player, 'Upon closer inspection, the cylinders are made of thick glass. Some of them are slightly cracked, and a thin vapor is emerging. Others are too frosted over to see within. In some, you see a dim silhouette.', 40);
-          case 'tubes':
-            return Broadcast.sayAt(player, 'The tubes are running from the alcove\'s entrance, across the ceiling, and into the wall above each of the cylinders. They are opaque, and you cannot tell what they may be filled with.');
-          default:
-            Broadcast.sayAt(player, 'Huh?');
+      updateTick: state => function () {
+        if (Random.probability(this.players.size || 0)) {
+          Broadcast.sayAt(this, 'You hear a soft, high whistling coming from the pipes overhead.');
         }
       }
     }
