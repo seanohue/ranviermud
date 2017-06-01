cf2e82c5016077948e7d46c80ad15c05e59db564
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2017-06-01 19:27:39 -0400

    Add flavorful items of destiny, and debounce the tomb events as well as adding multiples. For flavor.

--- bundles/myelin-areas/areas/spire.intro/scripts/rooms/1-tomb.js
+++ bundles/myelin-areas/areas/spire.intro/scripts/rooms/1-tomb.js
@@ -3,11 +3,26 @@
 module.exports = (srcPath) => {
   const Broadcast = require(srcPath + 'Broadcast');
   const Random = require(srcPath + 'RandomUtil');
+  let lastEventEmitted = Date.now();
   return {
     listeners: {
       updateTick: state => function () {
-        if (Random.probability(this.players.size || 0)) {
-          Broadcast.sayAt(this, '<white>You hear a soft, high whistling coming from the pipes overhead.</white>');
+        if (!this.players.size) {
+          return;
+        }
+
+        const cooldown = 20 * 1000;
+        const pastCooldown = (Date.now() - lastEventEmitted) > cooldown;
+        if (pastCooldown && Random.probability(1)) {
+          const flavorEvents = [
+            'You hear a soft, high whistling coming from the pipes overhead.',
+            'A wooshing sound can be heard from the chamber nearby.',
+            'You hear a scurrying noise coming from an indeterminate distance away.',
+            'A sound like distance thunder echoes from afar.',
+            'Bubbles form behind the glass of one of the cylinders.'
+          ];
+          Broadcast.sayAt(this, `<white>${Random.fromArray(flavorEvents)}</white>`);
+          lastEventEmitted = Date.now();
         }
       }
     }
