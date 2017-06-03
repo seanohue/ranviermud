299e59036f6b36609031d0736612ea1c1d8fdd7c
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2017-06-03 13:55:15 -0400

    Add some more items to the rooms, link it all together. Increase cooldown time for tomb events.

--- bundles/myelin-areas/areas/spire.intro/scripts/rooms/1-tomb.js
+++ bundles/myelin-areas/areas/spire.intro/scripts/rooms/1-tomb.js
@@ -3,7 +3,7 @@
 module.exports = (srcPath) => {
   const Broadcast = require(srcPath + 'Broadcast');
   const Random = require(srcPath + 'RandomUtil');
-  let lastEventEmitted = Date.now();
+  let lastEventEmitted = Date.now() - (60 * 1000);
   return {
     listeners: {
       updateTick: state => function () {
@@ -11,7 +11,7 @@ module.exports = (srcPath) => {
           return;
         }
 
-        const cooldown = 20 * 1000;
+        const cooldown = 60 * 1000;
         const pastCooldown = (Date.now() - lastEventEmitted) > cooldown;
         if (pastCooldown && Random.probability(1)) {
           const flavorEvents = [
