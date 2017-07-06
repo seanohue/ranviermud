bf8cde38a1ad8858b6bd1ab12ac6024b0a7f3fa2
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2017-07-06 12:59:35 -0400

    Have it display TOC if necessary on read cmd.

--- bundles/myelin-commands/behaviors/item/readable.js
+++ bundles/myelin-commands/behaviors/item/readable.js
@@ -14,10 +14,22 @@ module.exports = srcPath => {
         } = config;
 
         const contentMap = new Map(Object.entries(content));
-        const target = args ? args : contentMap.keys()[0];
+        if (!args || !args.length) {
+          return renderToC.call(this, player, content, divName)
+        }
         console.log(contentMap);
         console.log(target);
       },
     }
   };
+
+  function renderToC(player, content, divName) {
+    Broadcast.sayAt(player, Broadcast.center(40, this.name));
+    Broadcast.sayAt(player, this.description);
+    Broadcast.sayAt(player, `${divName}s: ${Array.from(content.keys()).map(String.prototype.toUpperCase).join(', ')}.`)
+    Broadcast.sayAt(player, `<b>Try 'read ${this.name.split(' ')[0].toLowerCase()}' [${divName}] to read a specific ${divName}</b>`);
+  }
+
 };
+
+
