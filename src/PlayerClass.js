c2cb6f72b620552d581418ae13d0d573f2776503
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2017-11-12 13:03:21 -0500

    Update player class src file to match new impl.'

--- src/PlayerClass.js
+++ src/PlayerClass.js
@@ -1,11 +1,13 @@
 'use strict';
 
-/**
- * This is an _example_ implementation of a base player class. This, along with
- * CommandParser is one of the few core classes Ranvier encourages you to
- * modify if you want more functionality. Almost all other features can be
- * overridden in bundles.
- */
+/** Classless system:
+ * The player in Myelin does not really have a class.
+ * Instead, they can develop their character to suit an
+ * archetype by boosting attributes and purchasing skills
+ * once they reach the skills' prerequisites.
+ * Player characters do have a "background" which determines
+ * only their starting attributes, skills, and equipment.
+*/
 class PlayerClass {
   /**
    * @param {string} id  id corresponding to classes/<id>.js file
@@ -31,23 +33,21 @@ class PlayerClass {
   }
 
   /**
-   * Table of level: abilities learned.
+   * Table of skill => prerequisites & their values.
    * Example:
-   *     {
-   *       1: { skills: ['kick'] },
-   *       2: { skills: ['bash'], spells: ['fireball']},
-   *       5: { skills: ['rend', 'secondwind'] },
-   *     }
-   * @type {Object<number, Array<string>>}
+   *   lunge: {
+   *      level:     4,
+   *      quickness:  12,
+   *      might: 12
+   *    },
+   * @type {Object<string, Object<string, number>>}
    */
   get abilityTable() {
     return this.config.abilityTable;
   }
 
   get abilityList() {
-    return Object.entries(this.abilityTable).reduce((acc, [ , abilities ]) => {
-      return acc.concat(abilities.skills || []).concat(abilities.spells || []);
-    }, []);
+    return Object.keys(this.abilityTable.skills);
   }
 
   /**
@@ -57,22 +57,64 @@ class PlayerClass {
    */
   getAbilitiesForPlayer(player) {
     let totalAbilities = [];
-    Object.entries(this.abilityTable).forEach(([level, abilities]) => {
-      if (level > player.level) {
-        return;
+    const abilities = Object.entries(this.abilityTable.skills);
+    for (const [ ability, prerequisites ] of abilities) {
+      const isEligible = this.determineEligibility(prerequisites, player);
+      if (isEligible) {
+        totalAbilities = totalAbilities.concat(ability);
       }
-      totalAbilities = totalAbilities.concat(abilities.skills || []).concat(abilities.spells || []);
-    });
+    }
     return totalAbilities;
   }
 
-  /**
-   * Check to see if this class has a given ability
-   * @param {string} id
-   * @return {boolean}
-   */
+  // Retrieve list of abilities that have been
+  // learned by this player.
+  getOwnAbilitiesForPlayer(player) {
+    return player.getMeta('abilities') || [];
+  }
+
+  /** Given a hash of prerequisites, determine if the player meets all of them or not.
+   * @param {Object<string,number>} prerequisites
+   * @param {Player}
+   * @return {Boolean} isEligible
+  */
+  determineEligibility(prerequisites, player) {
+    if (!prerequisites) {
+      return true;
+    }
+    const prereqList = Object.entries(prerequisites);
+    return prereqList.every(([ attribute, level ]) => {
+      switch(attribute) {
+        case 'cost':
+          const abilityPoints = parseInt(player.getMeta('abilityPoints'), 10) || 0;
+          return abilityPoints >= level;
+        case 'level':
+          return player.level >= level;
+        default:
+          return player.getBaseAttribute(attribute) >= level;
+      }
+    });
+  }
+
+  /** Does the ability even exist?
+   * @param {String} id
+   * @return {Boolean} exists
+  */
   hasAbility(id) {
     return this.abilityList.includes(id);
+  }
+
+/**
+   * Check if a player can buy a given ability
+   * @param {Player} player
+   * @param {string} abilityId
+   * @return {boolean}
+   */
+  canPurchaseAbility(player, abilityId) {
+    const ownAbilities = this.getOwnAbilitiesForPlayer(player);
+    return this.getAbilitiesForPlayer(player)
+               .includes(abilityId) &&
+               !ownAbilities.includes(abilityId);
   }
 
   /**
@@ -82,7 +124,8 @@ class PlayerClass {
    * @return {boolean}
    */
   canUseAbility(player, abilityId) {
-    return this.getAbilitiesForPlayer(player).includes(abilityId);
+    return this.getOwnAbilitiesForPlayer(player)
+               .includes(abilityId);
   }
 }
 
