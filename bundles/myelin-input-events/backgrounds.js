0f0de8e030377928caeb79ccabecdad72d5889d5
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2017-11-21 16:00:49 -0500

    Make id a string in background file.

--- bundles/myelin-input-events/backgrounds.js
+++ bundles/myelin-input-events/backgrounds.js
@@ -1,7 +1,7 @@
 module.exports = {
   tough: {
-    id: tough,
-    name: 'Rugged Tough',
+    id: "tough",
+    name: "Rugged Tough",
     description: "You've always done what it took to survive. Your fists and your prodigious size kept you alive, and earned you a living, even if it wasn't always honest. They usually deserved it, or so you told yourself.",
     attributes: {
       quickness: 11,
@@ -9,8 +9,9 @@ module.exports = {
       might: 17,
       willpower: 8
     },
-    equipment: ['spire.intro:12', 'spire.intro:13', 'spire.intro:14', 'spire.intro:15', 'spire.intro:16'],
-    skills: [ 'bash' ],
+    // Re-do but don't use numbers, yeesh
+    equipment: ["spire.intro:12", "spire.intro:13", "spire.intro:14", "spire.intro:15", "spire.intro:16"],
+    skills: [ "bash" ],
     attributePoints: 0,
     abilityPoints: 0,
     tier: 0
