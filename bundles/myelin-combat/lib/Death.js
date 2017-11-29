72705c887c0ec17e4a3e1184b8e5c0a659c9206d
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2017-11-29 00:03:11 -0500

    Update formula for memory points and also the death messages.

--- bundles/myelin-combat/lib/Death.js
+++ bundles/myelin-combat/lib/Death.js
@@ -1,16 +1,20 @@
 module.exports = srcPath => ({
   calculateMemories(player) {
-    return Math.floor(player.level / 5); // Basic implementation
+    const kills = player.getMeta('kills') || 0;
+    const strongest = (player.getMeta('strongestKilled') || {level: 0}).level;
+    return Math.floor(player.level / 5) +
+           Math.round(kills / 50) +
+           Math.round(strongest / 5);
   },
 
   message() {
     const Random = require(srcPath + './RandomUtil');
 
     return Random.fromArray([
-      "Ripozu en Paco.",
-      "You Died.",
-      "Keep Trying.",
-      "Death is not The End."
+      "You feel your soul rising from your body, to be spun into the ether once again",
+      "All thought ceases as life frees from your body.",
+      "You collapse in an unmoving heap.",
+      "One last blinding flash of pain and it is over."
     ]);
   }
 });
