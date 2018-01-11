df05a7297b6fd1c6cfd31ebfe613704903525e1c
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2018-01-10 23:24:28 -0500

    Working through some bugs.

--- bundles/myelin-input-events/input-events/finish-player.js
+++ bundles/myelin-input-events/input-events/finish-player.js
@@ -8,29 +8,31 @@ module.exports = (srcPath) => {
   const EventUtil = require(srcPath + 'EventUtil');
   const Player = require(srcPath + 'Player');
 
-
-
-  const backgrounds = (async function loadBackgrounds() {
+  let backgrounds = [];
+  (async function loadBackgrounds() {
     const fs = require('fs');
-    const yaml = require('yaml');
-    const readDirAsync = util.promisify(fs.readDir);
+    const util = require('util');
+    const yaml = require('js-yaml');
+    const path = require('path');
+
+    const readdirAsync = util.promisify(fs.readdir);
     const readFileAsync = util.promisify(fs.readFile);
-    const path = '../backgrounds/';
+    const pathToBg = path.resolve(__dirname, '../backgrounds');
 
-    const backgroundTitles = await readDirAsync(path);
-    const _backgrounds = await Promise.all(backgroundTitles)
-      .then(title => readFileAsync(`${path}${title}`))
-      .then(file => yaml.load(file)) // is this how to do???
-    console.log({_backgrounds})
-
-    return _backgrounds;
+    const backgroundTitles = await readdirAsync(pathToBg);
+    await Promise.all(backgroundTitles)
+      .then(files => files.map(file => console.log({file}) || readFileAsync(`${pathToBg}/${file}`)))
+      .then(files => files.map(promise => promise.then(file => yaml.load(file))))
+      .then(parsed => console.log({parsed}) || parsed.forEach(thing => console.log({thing}) || thing.then(bg => backgrounds.push(bg))))
   }());
 
   return {
     event: state => (socket, args) => {
       const { name, account, background = 'tough'} = args;
-      const backgroundObj = backgrounds.find(bg => bg.id === background);
-
+      if (background && typeof(background) !== 'object') {
+        background = backgrounds.find(bg => bg.id === background);
+      }
+      console.log({backgrounds, background});
       // TIP:DefaultAttributes: This is where you can change the default attributes for players
       // TODO:
       const attributes = Object.assign({
@@ -43,7 +45,7 @@ module.exports = (srcPath) => {
         willpower: 5,
         armor: 0,
         critical: 0
-      }, backgroundObj.attributes);
+      }, background.attributes);
 
       let player = new Player({
         name,
@@ -51,7 +53,7 @@ module.exports = (srcPath) => {
         attributes
       });
 
-      player.setMeta('background', background);
+      player.setMeta('background', background.id);
 
       args.account.addCharacter(name);
       args.account.save();
@@ -63,6 +65,7 @@ module.exports = (srcPath) => {
       player.save();
 
       // reload from manager so events are set
+      console.log({player}, player.name);
       player = state.PlayerManager.loadPlayer(state, player.account, player.name);
       player.socket = socket;
 
