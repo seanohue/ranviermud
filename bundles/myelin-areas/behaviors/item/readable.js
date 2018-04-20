83f234203ac925eec4a40b6f3c3250d604e0a3b5
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2018-04-20 15:40:10 -0400

    Add in a 'readable' behavior tentatively.

--- bundles/myelin-areas/behaviors/item/readable.js
+++ bundles/myelin-areas/behaviors/item/readable.js
@@ -3,37 +3,67 @@
 module.exports = srcPath => {
   const Broadcast = require(srcPath + 'Broadcast');
   const Logger = require(srcPath + 'Logger');
+  const Player = require(srcPath + 'Player');
+
   return {
     listeners: {
-      read: state => (config, player, args) => {
-        const title = this.name || this.roomDesc || 'Untitled';
-        const sectionsName = config.sectionsName || 'sections';
-        const prologue = config.prologue || '';
+      read: state => function (config, player, args) {
+        let {
+          divName = 'section',
+          content = {}
+        } = config;
+        const contentMap = new Map(Object.entries(content));
 
-        const argsList = args.split(' ');
-
-        if (player.__lastReadAttempt !== this.entityReference) {
-          player.__penultimateReadAttempt = player.__lastReadAttempt;
+        if (!args || !args.length) {
+          return renderTableOfContents.call(this, player, contentMap, divName)
         }
-        player.__lastReadAttempt = this.entityReference;
 
-        // Such as 'read bookName'
-        if (argsList.length === 0) {
-          Broadcast.sayAt(player, `<bold>${title}</bold>\n`);
-          Broadcast.sayAt(player, `${prologue}`, 40);
-          if (!(config.sections && config.sections.length) && config.text) {
-            throw new ReferenceError('Readable has no sections or text!');
-          }
-          for (const section in config.sections) {
+        const [targetDiv, toRead] = getContentToRead.call(this, contentMap, args, player);
 
-          }
-          // Title, Prologue, and TOC
+        if (toRead) {
+          return renderContent.call(this, toRead, divName, targetDiv, player);
+        }
 
-        } 
-
-        
-
-      }
+        Broadcast.sayAt(player, `<yellow>${this.name} does not have that ${divName}.</yellow>`);
+        return renderTableOfContents.call(this, player, contentMap, divName);
+      },
     }
+  };
+
+  function renderTableOfContents(player, content, divName) {
+    Broadcast.sayAt(player, Broadcast.center(40, this.name, 'bold', '-'));
+    Broadcast.sayAt(player, this.description);
+
+    const [first, ...rest] = divName;
+    const capitalizedDivName = [first.toUpperCase()].concat(rest).join('');
+    const divsList = Array
+      .from(content.keys())
+      .map(div => div.toUpperCase())
+      .join(', ');
+
+    const keywordForThis = this.name.split(' ')[0].toLowerCase();
+    Broadcast.sayAt(player, `<b>${capitalizedDivName}s:<b> ${divsList}.`)
+    Broadcast.sayAt(player, `<b>Try 'read ${keywordForThis} [${divName}]' to read a specific ${divName}.</b>`);
   }
-}
+
+  function getContentToRead(contentMap, args, player) {
+    if (args.length > 1) {
+      const found = args.filter(search => contentMap.has(search));
+      if (!found) {
+        const divNamesList = Array.from(contentMap.keys());
+        const partialMatch = args
+          .filter(search => divNamesList
+            .filter(key => key.includes(search)));
+        return [partialMatch[0], contentMap.get(partialMatch[0])];
+      }
+      return [found[0], contentMap.get(found[0])];
+    }
+    return [args[0], contentMap.get(args[0])];
+  }
+
+  function renderContent(divToRead, divName, divTargetName, player) {
+    Broadcast.sayAt(player, `<white><b>${this.name}:</white></b>`);
+    Broadcast.sayAt(player, Broadcast.center(40, `${divName.toUpperCase()} ${divTargetName.toUpperCase()}`, 'bold'));
+    return Broadcast.sayAt(player, divToRead);
+  }
+};
