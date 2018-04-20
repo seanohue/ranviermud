2b9d282ea93e27053c743fe1ea75c27b681750b4
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2018-04-20 00:46:06 -0400

    Add startquest behavior for starting a quest once a player enters a room.

---
+++ bundles/myelin-areas/behaviors/room/startquest.js
@@ -0,0 +1,28 @@
+'use strict';
+
+module.exports = (srcPath) => {
+  const Broadcast = require(srcPath + 'Broadcast');
+  const Logger = require(srcPath + 'Logger');
+  return  {
+    listeners: {
+      playerEnter: state => function (config, player) {
+        let questId;
+        if (typeof config === 'string') {
+          questId = config;
+        } else if (config && typeof config === 'object') {
+          questId = config.questId;
+        }
+
+        if (!questId) {
+          Logger.error(`No quest name configured in ${this.entityReference}`);
+          return;
+        }
+
+        const quest = state.QuestFactory.create(state, config.questId, player);
+        if (player.questTracker.canStart(quest)) {
+          player.questTracker.start(quest);
+        }
+      }
+    }
+  };
+};
