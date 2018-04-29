f35c3a1398009a41c37c1c934d5021228e9eda60
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2018-04-29 09:45:04 -0400

    Fix intro equipment stuff.

--- bundles/myelin-input-events/input-events/finish-player.js
+++ bundles/myelin-input-events/input-events/finish-player.js
@@ -15,8 +15,33 @@ module.exports = (srcPath) => {
         background = backgrounds.find(bg => bg.id === background);
       }
       // TIP:DefaultAttributes: This is where you can change the default attributes for players
+
+
+      console.log('Background is ', background);
+      /*
+        Background is  { id: 'mendicant',
+          name: 'Acrid Mendicant',
+          description: 'You\'re a wiley sort, surviving on the streets through your own wits, charming alms from strangers.',
+          attributes: { quickness: 6, intellect: 11, willpower: 12, might: 11 },
+          equipment: 
+          [ 'spire.intro:4',
+            'spire.intro:5',
+            'spire.intro:6',
+            'spire.intro:7',
+            'spire.intro:8' ],
+          skills: [ 'secondwind', 'concentration' ],
+          attributePoints: 0,
+          abilityPoints: 2,
+          tier: 0 }
+
+      */
+      args.account.addCharacter(name);
+      args.account.save();
+
+      const {name: bgName, abilityPoints, attributes, attributePoints,  equipment, skills} = background
+
       // TODO:
-      const attributes = Object.assign({
+      const defaultAttributes = Object.assign({
         health: 100,
         focus: 100,
         energy: 100,
@@ -26,20 +51,40 @@ module.exports = (srcPath) => {
         willpower: 5,
         armor: 0,
         critical: 0
-      }, background.attributes);
+      }, attributes);
 
       let player = new Player({
         name,
         account,
-        attributes
+        defaultAttributes
       });
 
-      player.setMeta('background', background.id);
+      //TODO: Custom descs
+      const desc = `${name} is here.`;
 
-      args.account.addCharacter(name);
-      args.account.save();
+      player.setMeta('class',            'base');
+      player.setMeta('background',       bgName);
+      player.setMeta('abilities',        skills);
+      player.setMeta('attributePoints',  attributePoints || 0);
+      player.setMeta('abilityPoints',    abilityPoints   || 0);
+      player.setMeta('description',      desc || '');
 
-      player.setMeta('class', 'base');
+      if (equipment && equipment.length) {
+        equipment.forEach(itemRef => {
+          const area = state.AreaManager.getAreaByReference(itemRef);
+          const item = state.ItemFactory.create(area, itemRef);
+          item.hydrate(state);
+          item.emit('get', player);
+          if (item.slot) {
+            player.equip(item);
+            item.emit('equip', player);
+          } else {
+            player.addItem(item);
+          }
+        });
+      }
+
+      player.save();
 
       const room = state.RoomManager.startingRoom;
       player.room = room;
