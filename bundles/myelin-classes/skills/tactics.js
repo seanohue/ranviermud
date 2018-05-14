c39dbd68c13ff307aaca0b1e568712aeb40162b8
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2018-05-14 15:18:08 -0400

    Add tactics skill.

---
+++ bundles/myelin-classes/skills/tactics.js
@@ -0,0 +1,38 @@
+'use strict';
+
+/**
+ * Passive combat-stat bonuses based on level.
+ */
+module.exports = (srcPath) => {
+
+  const SkillType = require(srcPath + 'SkillType');
+  const SkillFlag = require(srcPath + 'SkillFlag');
+
+  function getMagnitude(player) {
+    return Math.min(
+      Math.max(
+        player.getMaxAttribute('intellect') - 15,
+        Math.ceil(player.level / 2), 
+        2), 
+      10);
+  }
+
+  return {
+    name: 'Tactics',
+    type: SkillType.FEAT,
+    flags: [SkillFlag.PASSIVE],
+    effect: "skill.tactics",
+
+    configureEffect: effect => {
+      effect.state = Object.assign(effect.state, {
+        magnitude: getMagnitude(effect.target)
+      });
+
+      return effect;
+    },
+
+    info(player) {
+      return `Due to your tactical mind, you gain +${getMagnitude(player)} to armor and critical chance.`;
+    }
+  };
+};
