d4d53e19e8ba307de834e3e80d142c238ab243d2
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2018-05-14 20:38:33 -0400

    Add resource buff.

---
+++ bundles/myelin-classes/effects/resource.buff.js
@@ -0,0 +1,49 @@
+'use strict';
+
+// Increases XP and resource gathering amount.
+
+module.exports = srcPath => {
+  const Broadcast = require(srcPath + 'Broadcast');
+  const Flag = require(srcPath + 'EffectFlag');
+
+  return {
+    config: {
+      name: 'Resource Buff',
+      type: 'resource.buff',
+    },
+    flags: [Flag.BUFF],
+    listeners: {
+      reward(quest, player) {
+        const bonus = Math.round(LevelUtil.mobExp(quest.config.level) * .5);
+        Broadcast.sayAt(player, `You gain an experience bonus...`);
+        player.emit('experience', bonus);
+      },
+
+//      player.emit('currency', config.currency, amount);
+      currency(name, amount) {
+        const metaKey = `currency.${name}`;
+
+        const player = this.target;
+        const currentResource = player.getMeta(metaKey) || 0;
+        Broadcast.sayAt(player, `You are able to gather ${amount || 0} extra ${name}...`);
+        player.setMeta(metaKey, currentResource + amount);
+      }
+
+      gather(resource, amount = 0, name = 'resources') {
+        const metaKey = `resources.${name}`;
+
+        const player = this.target;
+        const currentResource = player.getMeta(metaKey) || 0;
+        Broadcast.sayAt(player, `You are able to gather ${amount || 0} extra ${name}...`);
+        player.setMeta(metaKey, currentResource + amount);
+      },
+
+      look(observer) {
+        const player = this.target;
+        if (observer.isNpc) { return; }
+        if (player.isNpc) { return; }
+        Broadcast.sayAt(observer, `Something about ${this.target.name} makes you want to give them a gift.`);
+      }
+    }
+  };
+};
