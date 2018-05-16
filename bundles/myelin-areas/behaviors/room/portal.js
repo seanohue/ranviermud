964963e5e694d3b2bfa616386c4c7db2a4ab5171
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2018-05-16 16:30:58 -0400

    Just commit a new area. Also add a try catch to portal code just in case.

--- bundles/myelin-areas/behaviors/room/portal.js
+++ bundles/myelin-areas/behaviors/room/portal.js
@@ -39,110 +39,116 @@ module.exports = srcPath => {
       },
 
       usePortal: state => function(_config, player, key) {
-        if (_config === true) _config = {};
+        try {
+          if (_config === true) _config = {};
 
-        const config = Object.assign(_config || {}, {
-          keyId: 'spire.intro:portalkey',
-          flags: []
-        });
+          const config = Object.assign(_config || {}, {
+            keyId: 'spire.intro:portalkey',
+            flags: []
+          });
 
-        if (key.entityReference !== config.keyId) {
-          return Broadcast.sayAt(player, '<yellow><b>This portal wants a different key...</b></yellow>');
-        }
-
-        if (player.isInCombat()) {
-          return Broadcast.sayAt(player, '<yellow><b>You cannot use the portal while fighting.</b></yellow>');
-        }
-
-        Broadcast.sayAt(player, `<b>The portal emits a low hum...</b>`);
-        Broadcast.sayAtExcept(this, `<b>The portal emits a low hum as ${player.name} uses their key...</b>`, player);
-
-        Broadcast.sayAt(player, `<b>There is a <yellow>flash</yellow> of light, and your surroundings vanish...</b>`);
-        Broadcast.sayAtExcept(this, `<b>${player.name} disappears in a <yellow>flash</yellow> of light...</b>`, player);
-
-        // Have it remove the player from the room, broadcasting such to them and anyone else there.
-        //TODO: Do this to entire party... Have them in a limbo of sorts? Have them vote? Idk.
-        this.removePlayer(player);
-        player._isUsingPortal = true;
-
-        if (!PortalDestinations.size) {
-          // LOAD
-          if (!loaded) {
-            for (const [areaName, area] of state.AreaManager.areas) {
-              if (!area.info.isGenerated) continue;
-              PortalDestinations.set(areaName, area);
-            }
-            loaded = true;
+          if (!key || key.entityReference !== config.keyId) {
+            return Broadcast.sayAt(player, '<yellow><b>This portal wants a different key...</b></yellow>');
           }
+
+          if (player.isInCombat()) {
+            return Broadcast.sayAt(player, '<yellow><b>You cannot use the portal while fighting.</b></yellow>');
+          }
+
+          Broadcast.sayAt(player, `<b>The portal emits a low hum...</b>`);
+          Broadcast.sayAtExcept(this, `<b>The portal emits a low hum as ${player.name} uses their key...</b>`, player);
+
+          Broadcast.sayAt(player, `<b>There is a <yellow>flash</yellow> of light, and your surroundings vanish...</b>`);
+          Broadcast.sayAtExcept(this, `<b>${player.name} disappears in a <yellow>flash</yellow> of light...</b>`, player);
+
+          // Have it remove the player from the room, broadcasting such to them and anyone else there.
+          //TODO: Do this to entire party... Have them in a limbo of sorts? Have them vote? Idk.
+          this.removePlayer(player);
+          player._isUsingPortal = true;
 
           if (!PortalDestinations.size) {
-            return generateDestination();
-          }
-        }
-        
-        if (PortalDestinations.size > 1 || !PortalDestinations.has(player.room.area.name)) {
-          const destinationValues = Array.from(PortalDestinations.values());
-          const inLevelRange = destinationValues.filter((dest) => {
-            return area.name !== player.room.area.name && 
-              dest.levelRange 
-                ? player.level >= dest.levelRange.min && player.level <= dest.levelRange.max
-                : true;
-          });
-
-          if (!inLevelRange.length) {
-            return generateDestination();
-          } else {
-            const destinationArea = Random.fromArray(inLevelRange);
-            const destinationRoom = Array.from(destinationArea.rooms.values())[0];
-            return movePlayerToPortalDestination(destinationRoom.entityReference);
-          }
-        }
-
-        function generateDestination() {
-          console.log('GENERATING NEW AREA....');
-
-          return generate(srcPath, state, player.level)
-            .then(({generated, name}) => {
-              console.log('Generated!');
-              const {firstRoom} = addToWorld(srcPath, state, name, generated);
-
-              movePlayerToPortalDestination(firstRoom);
-            })
-            .catch(err => {
-              console.error('Error', err);
-              player._isUsingPortal = false;
-              Broadcast.sayAt(player, 'An error has occurred. Please contact an admin.');
-            });
-        }
-
-        function movePlayerToPortalDestination(firstRoom) {
-          console.log({firstRoom});
-          const targetRoom = state.RoomManager.getRoom(firstRoom);
-          if (!targetRoom) {
-            return Broadcast.sayAt(player, 'Teleportation failed. No such room entity reference exists. Contact an admin.');
-          } else if (targetRoom === player.room) {
-            return Broadcast.sayAt(player, 'Teleportation failed. Teleported to same room. Contact an admin.');
-          }
-
-          player.followers.forEach(follower => {
-            // TODO: Change to send followers as well.
-            follower.unfollow();
-            if (!follower.isNpc) {
-              Broadcast.sayAt(follower, `You stop following ${player.name}.`);
+            // LOAD
+            if (!loaded) {
+              for (const [areaName, area] of state.AreaManager.areas) {
+                if (!area.info.isGenerated) continue;
+                PortalDestinations.set(areaName, area);
+              }
+              loaded = true;
             }
-          });
-    
-          if (player.isInCombat()) {
-            player.removeFromCombat();
+
+            if (!PortalDestinations.size) {
+              return generateDestination();
+            }
           }
-    
-          player.moveTo(targetRoom, () => {
-            Broadcast.sayAt(player, '<b><green>You find yourself in a strange new world...</green></b>\r\n');
-            Broadcast.sayAtExcept(targetRoom, `<b>${player.name} appears in a <yellow>flash</yellow> of light.</b>`, player);
-            state.CommandManager.get('look').execute('', player);
-            player._isUsingPortal = false;
-            state.ItemManager.remove(key);
-          });
+          
+          if (PortalDestinations.size > 1 || !PortalDestinations.has(player.room.area.name)) {
+            const destinationValues = Array.from(PortalDestinations.values());
+            const inLevelRange = destinationValues.filter((dest) => {
+              return area.name !== player.room.area.name && 
+                dest.levelRange 
+                  ? player.level >= dest.levelRange.min && player.level <= dest.levelRange.max
+                  : true;
+            });
+
+            if (!inLevelRange.length) {
+              return generateDestination();
+            } else {
+              const destinationArea = Random.fromArray(inLevelRange);
+              const destinationRoom = Array.from(destinationArea.rooms.values())[0];
+              return movePlayerToPortalDestination(destinationRoom.entityReference);
+            }
+          }
+
+          function generateDestination() {
+            console.log('GENERATING NEW AREA....');
+
+            return generate(srcPath, state, player.level)
+              .then(({generated, name}) => {
+                console.log('Generated!');
+                const {firstRoom} = addToWorld(srcPath, state, name, generated);
+
+                movePlayerToPortalDestination(firstRoom);
+              })
+              .catch(err => {
+                console.error('Error', err);
+                player._isUsingPortal = false;
+                Broadcast.sayAt(player, 'An error has occurred. Please contact an admin.');
+              });
+          }
+
+          function movePlayerToPortalDestination(firstRoom) {
+            console.log({firstRoom});
+            const targetRoom = state.RoomManager.getRoom(firstRoom);
+            if (!targetRoom) {
+              return Broadcast.sayAt(player, 'Teleportation failed. No such room entity reference exists. Contact an admin.');
+            } else if (targetRoom === player.room) {
+              return Broadcast.sayAt(player, 'Teleportation failed. Teleported to same room. Contact an admin.');
+            }
+
+            player.followers.forEach(follower => {
+              // TODO: Change to send followers as well.
+              follower.unfollow();
+              if (!follower.isNpc) {
+                Broadcast.sayAt(follower, `You stop following ${player.name}.`);
+              }
+            });
+      
+            if (player.isInCombat()) {
+              player.removeFromCombat();
+            }
+      
+            player.moveTo(targetRoom, () => {
+              Broadcast.sayAt(player, '<b><green>You find yourself in a strange new world...</green></b>\r\n');
+              Broadcast.sayAtExcept(targetRoom, `<b>${player.name} appears in a <yellow>flash</yellow> of light.</b>`, player);
+              state.CommandManager.get('look').execute('', player);
+              player._isUsingPortal = false;
+              state.ItemManager.remove(key);
+            });
+          }
+        } catch (e) {
+          Broadcast.sayAt(player, 'Error: ' + e);
+          Broadcast.sayAt(player, 'Please concact an Admin!');
+          player._isUsingPortal = false;
         }
       }
     }
