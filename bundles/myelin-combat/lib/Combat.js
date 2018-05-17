16c701cf9bc29113bed4e8c1ff10d3cf91f3c6fa
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2018-05-17 16:55:54 -0400

    Make console log useful.

--- bundles/myelin-combat/lib/Combat.js
+++ bundles/myelin-combat/lib/Combat.js
@@ -146,7 +146,7 @@ class Combat {
    * @param {Character} target
    */
   static makeAttack(attacker, target) {
-    console.log(attacker.effects);
+    console.log(attacker.name, attacker.effects.effects);
     const rawDamageAmount = attacker.hasEffectType('skill:stun') ? 0 : this.calculateWeaponDamage(attacker);
     const isPsionic = attacker.metadata && attacker.metadata.damageType === 'psionic';
     const amount = Math.max(rawDamageAmount - this.calculateDefense(target, rawDamageAmount, isPsionic), 0);
