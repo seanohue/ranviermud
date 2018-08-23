e040d75d0073168183ec03aafef6c86c1871b803
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2018-08-22 21:58:10 -0400

    Fix seecombatants.

--- bundles/debug-commands/commands/seecombatants.js
+++ bundles/debug-commands/commands/seecombatants.js
@@ -3,11 +3,12 @@
 module.exports = (srcPath) => {
   const Broadcast = require(srcPath + 'Broadcast');
   const PlayerRoles = require(srcPath + 'PlayerRoles');
-
+  const Logger = require(srcPath + 'Logger');
   return {
     requiredRole: PlayerRoles.ADMIN,
     command: (state) => (args, player) => {
-      const list = [...player.combatants].map(c => c.name).join(', ');
+      let list = [...player.combatants].map(c => c.name).join(', ');
+      if(!list) list = 'No one.';
       Broadcast.sayAt(player, list);
       Logger.warn('Combatants of ' + player.name + ': ' + list);
     }
