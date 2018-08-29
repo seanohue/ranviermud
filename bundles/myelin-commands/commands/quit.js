5fc0e7d42b385bcf6b2aadc8b0fafdd07e24135c
Sean <sean.odonohue@detroitlabs.com>
2018-08-29 15:19:29 -0400

    Update quit.js

--- bundles/myelin-commands/commands/quit.js
+++ bundles/myelin-commands/commands/quit.js
@@ -5,7 +5,7 @@ module.exports = (srcPath) => {
 
   return {
     usage: 'quit',
-    aliases: ['signoff', 'logout', 'signout'],
+    aliases: ['signoff', 'logout', 'signout', 'logoff'],
     command: (state) => (args, player) => {
       if (player.isInCombat()) {
         return Broadcast.sayAt(player, "You're too busy fighting for your life!");
